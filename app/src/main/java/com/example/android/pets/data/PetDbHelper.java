62d32fa6a0d0a6ba1f6d081ba80778ff067346f0
r4dixx <amael.sikel@gmail.com>
2018-07-22 22:53:03 +0200

    :construction: SQLiteOpenHelper to create database
    - created PetDbHelper extending SQLiteOpenHelper
    - with constants for database name and version
    - with constructor
    - with onCreate method containing table creation that gets executed at the end of the onCreate
    
    - updated CatalogActivity to display database info in the onCreate

---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,35 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.pets.data.PetContract.PetEntry;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "shelter.db";
+    private static final int DB_VERSION = 1;
+
+    public PetDbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PETS_TABLE =
+                "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                        + PetEntry._ID + " INTEGER" + " PRIMARY KEY AUTOINCREMENT, "
+                        + PetEntry.COLUMN_NAME + " TEXT" + " NOT NULL, "
+                        + PetEntry.COLUMN_BREED + " TEXT, "
+                        + PetEntry.COLUMN_GENDER + " INTEGER" + " NOT NULL, "
+                        + PetEntry.COLUMN_WEIGHT + " INTEGER" + " NOT NULL" + " DEFAULT 0);";
+
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
